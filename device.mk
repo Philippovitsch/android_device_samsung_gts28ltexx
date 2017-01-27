279fbeaa348b9c725eba8dcb0c78eeea41538c66
Philippovitsch <philippst@gmx.at>
2017-01-27 22:20:57 +0100

    Sanitize makefile comments

--- device.mk
+++ device.mk
@@ -1,23 +1,14 @@
 LOCAL_PATH := device/samsung/gts28ltexx
 
-###########################################################
-### RAMDISK
-###########################################################
-
+# Ramdisk
 PRODUCT_PACKAGES += \
     init.baseband.rc
 
-###########################################################
-### AUDIO
-###########################################################
-
+# Tinyalsa
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-###########################################################
-### BLUETOOTH
-###########################################################
-
+# Bluetooth
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
 
